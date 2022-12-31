b08269d08a0d8480af63c0c3223c0b9d373d2863
AIexC <46251492+AIexC@users.noreply.github.com>
2022-12-31 16:01:05 +0100

    renaming of variables

--- src/filename_helpers.py
+++ src/filename_helpers.py
@@ -4,18 +4,18 @@ SUPPORTED_IMAGE_SUFFIXES = ["jpg"]
 SUPPORTED_VIDEO_SUFFIXES = ["mp4"]
 
 
-def get_lower_case_file_suffix(file_name):
-    file_suffix = file_name.split('.')[-1].lower()
+def get_lower_case_file_suffix(path):
+    file_suffix = path.split('.')[-1].lower()
     return file_suffix
 
 
-def is_image(file_name):
-    file_suffix = get_lower_case_file_suffix(file_name)
+def is_image(path):
+    file_suffix = get_lower_case_file_suffix(path)
     return file_suffix in SUPPORTED_IMAGE_SUFFIXES
 
 
-def is_video(file_name):
-    file_suffix = get_lower_case_file_suffix(file_name)
+def is_video(path):
+    file_suffix = get_lower_case_file_suffix(path)
     return file_suffix in SUPPORTED_VIDEO_SUFFIXES
 
 
@@ -24,10 +24,10 @@ def get_versioned_file_path(path):
         return path
 
     filename, extension = os.path.splitext(path)
-    counter = 2
+    version_conter = 2
 
     while os.path.exists(path):
-        path = filename + "_v" + str(counter) + "" + extension
-        counter += 1
+        path = filename + "_v" + str(version_conter) + extension
+        version_conter += 1
 
     return path
