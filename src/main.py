3e28c81b5ed1f2ea659bfac7273dcd2c2e28aa29
AIexC <46251492+AIexC@users.noreply.github.com>
2022-12-31 01:09:12 +0100

    pu all logs in a log dir

--- src/main.py
+++ src/main.py
@@ -12,11 +12,14 @@ LOG_LEVEL_INFO = "INFO"
 LOG_LEVEL_DEBUG = "DEBUG"
 LOG_LEVELS = [LOG_LEVEL_INFO, LOG_LEVEL_DEBUG]
 
+LOG_DIR = "reel-maker-logs"
 
 def setup_logger(loglevel):
+
+    os.makedirs(LOG_DIR, exist_ok=True)
     now_string = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     logfile = f'make_reel_{now_string}.log'
-    logging.basicConfig(filename=logfile, level=loglevel)
+    logging.basicConfig(filename=os.path.join(LOG_DIR, logfile), level=loglevel)
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 
