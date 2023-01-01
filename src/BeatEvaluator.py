387fad0ae4adf17bc4f2e429599e1d0d1a629b2e
AIexC <46251492+AIexC@users.noreply.github.com>
2023-01-01 12:29:59 +0100

    extract wav converter

--- src/BeatEvaluator.py
+++ src/BeatEvaluator.py
@@ -7,9 +7,8 @@ import wave
 import librosa
 import numpy as np
 from IPython import display as ipd
-from moviepy import editor as mpe
-from pydub import AudioSegment
 
+from WavConverter import WavConverter
 from helpers import remove_file
 
 
@@ -19,7 +18,7 @@ class BeatEvaluator:
     def evaluate_beat_times_from_audio_file(path_to_audio_file):
 
         path_to_temp_wav = BeatEvaluator.__get_path_to_temp_wav(path_to_audio_file)
-        BeatEvaluator.__create_temp_wav(path_to_audio_file, path_to_temp_wav)
+        WavConverter.convert(path_to_audio_file, path_to_temp_wav)
         beat_times = BeatEvaluator.__evaluate_beat_times_from_wav(path_to_temp_wav)
         remove_file(path_to_temp_wav)
 
@@ -57,27 +56,6 @@ class BeatEvaluator:
         temp_wav_file = "TEMP_" + basename + ".wav"
         path_to_temp_wav_file = os.path.join(workdir, temp_wav_file)
         return path_to_temp_wav_file
-
-    @staticmethod
-    def __create_temp_wav(path_to_audio_file, path_to_temp_wav):
-        remove_file(path_to_temp_wav)
-
-        logging.info(f"About to convert {path_to_audio_file} to {path_to_temp_wav}...")
-
-        if path_to_audio_file.endswith(".wav"):
-            logging.info("Audio file is .wav, so copy it to tempdir...")
-            raise NotImplementedError("Must be implemented")
-        elif path_to_audio_file.endswith(".mp4"):
-            my_clip = mpe.VideoFileClip(path_to_audio_file)  # r"downloaded_reel.mp4"
-            my_clip.audio.write_audiofile(path_to_temp_wav)
-            logging.info("Finished conversion!")
-        elif path_to_audio_file.endswith(".mp3"):
-            # ToDo: not sure if this works already...
-            sound = AudioSegment.from_mp3(path_to_audio_file)
-            sound.export(path_to_temp_wav, format="wav")
-            logging.info("Finished conversion!")
-        else:
-            raise NotImplementedError(f"Filetype of {path_to_audio_file} not supported for beat time evaluation")
 
     @staticmethod
     def __evaluate_beat_times_from_wav(wav_file):
---
+++ src/WavConverter.py
@@ -0,0 +1,54 @@
+import logging
+import shutil
+
+from moviepy import editor as mpe
+# ffprobe or are avprobe required for mp3 conversion, see https://deviloper.in/mp3-to-wav-file-conversion-using-python
+from pydub import AudioSegment
+
+from filename_helpers import get_lower_case_file_suffix
+from helpers import remove_file
+
+WAV_SUFFIX = "wav"
+MP3_SUFFIX = "mp3"
+MP4_SUFFIX = "mp4"
+
+SUPPORTED_INPUTS = [MP3_SUFFIX, MP4_SUFFIX]
+
+
+class WavConverter:
+
+    @staticmethod
+    def convert(input_file, output_file):
+        if get_lower_case_file_suffix(output_file) != WAV_SUFFIX:
+            raise IOError(f"Invalid name for output file '{output_file}'. Must must have suffix '{WAV_SUFFIX}'. ")
+
+        remove_file(output_file)
+
+        logging.info(f"About to convert {input_file} to {output_file}...")
+
+        if get_lower_case_file_suffix(input_file) == WAV_SUFFIX:
+            shutil.copyfile(input_file, output_file)
+            logging.info("Input is .wav, so just copied it.")
+            return
+
+        if get_lower_case_file_suffix(input_file) == "mp4":
+            WavConverter.__convert_from_mp4(input_file, output_file)
+        elif get_lower_case_file_suffix(input_file) == "mp3":
+            WavConverter.__convert_from_mp3(input_file, output_file)
+
+        else:
+            raise IOError(f"Filetype of {input} not supported for conversion to wav. "
+                          f"File must be of type {'/'.join(SUPPORTED_INPUTS)}.")
+
+        logging.info("Finished conversion!")
+
+    @staticmethod
+    def __convert_from_mp3(input_file, output_file):
+        sound = AudioSegment.from_mp3(
+            input_file)
+        sound.export(output_file, format="wav")
+
+    @staticmethod
+    def __convert_from_mp4(input_file, output_file):
+        my_clip = mpe.VideoFileClip(input_file)
+        my_clip.audio.write_audiofile(output_file)
